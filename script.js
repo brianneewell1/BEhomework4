f1962e05334e97e657d9ae7d89d74331d51f1865
Brianne <brianne.ewell1@gmail.com>
2020-10-07 16:08:17 -0600

    updated JS variables, added hidePage function

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-var timeEl = document.querySelector("badge badge-light");
-
-var secondsLeft = 180;
-
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft;
-
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage("Game is over");
+let questionArray = [
+    question1 = {
+        Question: "What is my name?",
+        CorrectAnswer: "correct answer",
+        AllAnswer: [
+            "randomAnswer",
+            "wrongAnswer",
+            "anotherAnswer",
+            "correct answer"
+        ]
     }
+]
 
-  }, 1000);
+/************ Global Variables */
+
+var highScore = [];
+
+var viewScore = document.getElementById("viewScore");
+var time = document.getElementById("time");
+var quizPage = document.getElementById("quizPage");
+var question = document.getElementById("question");
+var answer = document.getElementById("answer");
+var startButton = document.getElementById("startButton");
+var finalScore = document.getElementById("finalScore");
+var initialInput = document.getElementById("initialInput");
+var submitInitials = document.getElementById("initialInput");
+var goBack = document.getElementById("goBack");
+var clearScore = document.getElementById("clearScore");
+
+var highscorePage = document.getElementById("highScorePage");
+var wrapPage = document.getElementById("wrapPage");
+var initialsPage = document.getElementById("initialsPage");
+var quizPage = document.getElementById("quizPage");
+
+let pageArray = [highScore, wrapPage, initialsPage, quizPage];
+
+/****Timer Start */
+startButton.addEventListener("click", runTime)
+
+
+/***Hide Pages */
+function hidePages() {
+    for (let i = 0; i < pageArray.length; i++) {
+        if (!pageArray[i].classList.contains('hide')) {
+            pageArray[i].classList.add('hide');
+        }
+    }
 }
-setTime();
+
+
+function runTime() {
+    hidePages();
+    quizPage.classList.remove('hide');
+}
