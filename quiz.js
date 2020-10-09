c63a34faf1a825267ac9575f9e94a08aa2e77f26
Brianne <brianne.ewell1@gmail.com>
2020-10-08 18:00:45 -0600

    started over with new code

---
+++ quiz.js
@@ -0,0 +1,153 @@
+// select all elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
+const initials = document.getElementById("initials");
+
+// create our questions
+let questions = [
+    {
+        question: "How old was Nicholas Flamel when he decided to destroy the Sorcerer's Stone?",
+        choiceA: "665",
+        choiceB: "682",
+        choiceC: "865",
+        correct: "A"
+    }, {
+        question: "Dumbledore has a scare above his left knee that is a perfect map of what?",
+        choiceA: "Diagon Alley",
+        choiceB: "The London Underground",
+        choiceC: "King's Cross Station",
+        correct: "B"
+    }, {
+        question: "Where does Vernon Dursley work?",
+        choiceA: "Mason Drilling Company",
+        choiceB: "Droonings",
+        choiceC: "Grunnings",
+        correct: "C"
+    }
+];
+
+// create some variables
+
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 45;
+const quizTime = 45; // 45s
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / quizTime;
+let TIMER;
+let score = 0;
+
+// render a question
+function renderQuestion() {
+    let q = questions[runningQuestion];
+
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+start.addEventListener("click", startQuiz);
+
+// start quiz
+function startQuiz() {
+    start.style.display = "none";
+    renderQuestion();
+    title.style.display = "none";
+    subtitle.style.display = "none";
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
+}
+
+// render progress
+function renderProgress() {
+    for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+        progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+    }
+}
+
+// counter render
+
+function renderCounter() {
+    if (count <= quizTime) {
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count--;
+    } else {
+        count = 0;
+        // change progress color to red
+        answerIsWrong();
+        if (runningQuestion < lastQuestion) {
+            answerIsWrong();
+        } else {
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+// checkAnswer
+
+function checkAnswer(answer) {
+    if (answer == questions[runningQuestion].correct) {
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    }
+if 
+    // answer is wrong
+    // change progress color to red
+    //subtract time
+    (answer != questions[runningQuestion].correct) {
+        count--;
+    
+    answerIsWrong();
+}
+
+if (runningQuestion < lastQuestion) {
+    runningQuestion++;
+    renderQuestion();
+} else {
+    // end the quiz and show the score
+    clearInterval(TIMER);
+    scoreRender();
+    enterInitials();
+}
+}
+
+// answer is correct
+function answerIsCorrect() {
+    document.getElementById(runningQuestion).style.backgroundColor = "green";
+}
+
+// answer is Wrong
+function answerIsWrong() {
+    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
+
+// score render
+function scoreRender() {
+    scoreDiv.style.display = "block";
+
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score / questions.length);
+
+    scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
+}
+
+//enter initials
+function enterInitials(){
+    initials.style.display = "block";
+}
--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-let questionArray = [
+let questions = [
     question1 = {
         Question: "What is my name?",
-        CorrectAnswer: "correct answer",
-        AllAnswer: [
-            "randomAnswer",
-            "wrongAnswer",
-            "anotherAnswer",
-            "correct answer"
-        ]
+        choiceA: "correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+        }
+    question2 = {
+        Question: "What is my name?",
+        choiceA: "wrong",
+        choiceB : "Wrong",
+        choiceC : "correct",
+        correct : "C"
     }
+    question3 = {
+        Question: "What is my name?",
+        choiceA: "wrong",
+        choiceB : "Wrong",
+        choiceC : "correct",
+        correct : "C"
 ]
+
+const lastQuestion = questions.length -1;
+let runningQuestion = 0;
+
+/***Render a Question */
+function renderQuestion(){
+    let q = questions[runningQuestion];
+
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
 
 /************ Global Variables */
 
@@ -35,20 +58,37 @@ var quizPage = document.getElementById("quizPage");
 let pageArray = [highScore, wrapPage, initialsPage, quizPage];
 
 /****Timer Start */
-startButton.addEventListener("click", runTime)
-
+startButton.addEventListener("click", startQuiz);
+startButton.addEventListener("click", runTime);
 
 /***Hide Pages */
-function hidePages() {
-    for (let i = 0; i < pageArray.length; i++) {
-        if (!pageArray[i].classList.contains('hide')) {
-            pageArray[i].classList.add('hide');
-        }
+function hidePages(pageArray: []) {
+    var x = document.getElementById("highscorePage");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
 }
 
-
+/***Timer */
 function runTime() {
     hidePages();
     quizPage.classList.remove('hide');
+}
+
+var timeEl = document.getElementById("time");
+var secondsLeft = 180;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage("Time is up");
+    }
+
+  }, 1000);
 }
